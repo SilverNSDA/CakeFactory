884ff33ed62e86847678b1c7df8207522bc614fb
SilverNSDA <HatoriNSDA@gmail.com>
2017-12-22 17:09:31 +0700

    create model controller

---
+++ resources/views/include/navigation.blade.php
@@ -0,0 +1,12 @@
+<div>
+    <nav>
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+                        <a href="{{ route('register') }}">Register</a>
+                    @endauth
+                </div>
+    </nav>
+</div>
